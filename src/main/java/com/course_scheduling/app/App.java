6d1d13cd3b89e9c5a15be48000692bb9e3c32229
root <root@LAPTOP-B47NU6F4.localdomain>
2021-12-08 00:27:41 +0100

    chore: java project initialization, write a simple installation guide, changed the .gitignore to fit the project

---
+++ src/main/java/com/course_scheduling/app/App.java
@@ -0,0 +1,16 @@
+package com.course_scheduling.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+}
+
+
