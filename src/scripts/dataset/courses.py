6ffffebf19c02e5515df570e9909a637b6cfdd10
Argi K <argikartika@gmail.com>
2021-12-19 20:20:56 +0100

    feat: added major to courses.py

--- src/scripts/dataset/courses.py
+++ src/scripts/dataset/courses.py
@@ -4,26 +4,31 @@ import pandas as pd
 from random import randrange, choice
 import csv
 
-# get project root, relative to this folder: if the folder changes, the path MUST be changed as well. 
+# get project root, relative to this folder: if the folder changes, the path MUST be changed as well.
 PROJECT_ROOT = Path().resolve().parent.parent.parent
 
-#initialize the directory
+# initialize the directory
 os.chdir(PROJECT_ROOT)
-datasetUnitimeFilePath = Path('dataset/raw','dataset_pu-fal07-llr.xlsx')
-datasetIllinoisFilePath = Path('dataset/raw','dataset_illinois_2019-fa.csv')
-instructorFilePath = Path('dataset/processed','instructors.csv')
+datasetUnitimeFilePath = Path('dataset/raw', 'dataset_pu-fal07-llr.xlsx')
+datasetIllinoisFilePath = Path('dataset/raw', 'dataset_illinois_2019-fa.csv')
+instructorFilePath = Path('dataset/processed', 'instructors.csv')
 
 # read unitime dataset file
-datasetUnitime = pd.read_excel(datasetUnitimeFilePath, sheet_name='Sheet1', usecols="AO, AP, AQ")
+datasetUnitime = pd.read_excel(
+    datasetUnitimeFilePath, sheet_name='Sheet1', usecols="AO, AP, AQ")
 datasetUnitime.dropna(inplace=True)
 
 # read instructor file
-instructorFile = pd.read_csv(instructorFilePath, usecols = ['instructors'], sep=";")
+instructorFile = pd.read_csv(instructorFilePath, usecols=[
+                             'instructors'], sep=";")
 instructors = instructorFile["instructors"].to_list()
 
 # read illinois dataset file
-datasetIllinois = pd.read_csv(datasetIllinoisFilePath, usecols = ['Name'], sep=",").drop_duplicates()
+datasetIllinois = pd.read_csv(datasetIllinoisFilePath, usecols=[
+                              'Name', "CRN"], sep=",").drop_duplicates()
 coursesNames = datasetIllinois['Name'].to_list()
+coursesDepts = datasetIllinois['CRN'].to_list()
+
 
 # find how many times the courses are scheduled weekly
 # AO (days) column stores the number of days the courses are held
@@ -31,37 +36,51 @@ coursesNames = datasetIllinois['Name'].to_list()
 def findWeeklyHours():
     weeklySchedules = datasetUnitime['days']
     durationInFiveMinutesUnit = datasetUnitime['length']
-    weeklySchedulesCounts = list(map(lambda schedule:str(schedule).count("1"), weeklySchedules))
-    durationInHours = list(map(lambda hours:hours/12, durationInFiveMinutesUnit))
+    weeklySchedulesCounts = list(
+        map(lambda schedule: str(schedule).count("1"), weeklySchedules))
+    durationInHours = list(
+        map(lambda hours: hours/12, durationInFiveMinutesUnit))
 
     weeklyHours = list()
     for index, scheduleCount in enumerate(weeklySchedulesCounts):
         weeklyHours.append(scheduleCount*durationInHours[index])
-    
+
     return weeklyHours
+
 
 # generate weekly hours
 weeklyHours = findWeeklyHours()
 
-# get courses name from illinois dataset and limit the length to the number of courses in Unitime
-courses = coursesNames[:len(weeklyHours)]
+# get courses, removes &amp;, limit the length, remove duplicates 
+uniqueCoursesDepts = list()
+filteredCourses = list()
+for index, course in enumerate(coursesNames):
+    if course not in filteredCourses: 
+        filteredCourses.append(course)
+        uniqueCoursesDepts.append([course.replace("&amp;", "&"), coursesDepts[index]])
+
+# separate the zipped courses & dept, get the first two digit of CRN as department names
+courses, depts=zip(*uniqueCoursesDepts[:len(weeklyHours)])
+courses, depts=(list(courses),list(depts))
+depts = list(map(lambda dept: str(dept)[:2], coursesDepts[:len(weeklyHours)]))
 
 # generate a list of course instructor ids
 courseInstructors = list()
-for i in range (0, len(weeklyHours)):
+for i in range(0, len(weeklyHours)):
     courseInstructors.append(randrange(0, len(instructors), 1))
 
-#Generate new csv file
+# Generate new csv file
 targetFilePath = Path("dataset/processed", "courses.csv")
 targetFile = open(targetFilePath, 'w', newline="")
 
-# write the file and close 
+# write the file and close
 writer = csv.writer(targetFile, dialect="excel", delimiter=";")
-writer.writerow(["courses", "weekly_hours", "instructors"])
+writer.writerow(["courses", "weekly_hours", "instructors", "major"])
 i = 0
-while i!=len(courses):
-    writer.writerow([courses[i:i+1], weeklyHours[i:i+1], courseInstructors[i:i+1]])
-    i+=1
+while i != len(courses):
+    writer.writerow([courses[i:i+1], weeklyHours[i:i+1],
+                    courseInstructors[i:i+1], depts[i:i+1]])
+    i += 1
 targetFile.close()
 
 # replace quotation marks
@@ -69,9 +88,6 @@ with open(targetFilePath, "r+", encoding="utf-8") as csv_file:
     content = csv_file.read()
 
 with open(targetFilePath, "w+", encoding="utf-8") as csv_file:
-    newContent = content.replace('"', '').replace("[", "").replace("]", "").replace("\'", "").replace(", ", ",")
+    newContent = content.replace('"', '').replace(
+        "[", "").replace("]", "").replace("\'", "").replace(", ", ",")
     csv_file.write(newContent)
-
-    
-
-
