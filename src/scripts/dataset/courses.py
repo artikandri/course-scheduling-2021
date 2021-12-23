39dbe3009c8248f9d56c20a5318a1666de05a41d
Argi K <argikartika@gmail.com>
2021-12-23 02:56:11 +0100

    feat: revised courses.py, instructors.py, rooms.py

--- src/scripts/dataset/courses.py
+++ src/scripts/dataset/courses.py
@@ -14,28 +14,35 @@ datasetIllinoisFilePath = Path('dataset/raw', 'dataset_illinois_2019-fa.csv')
 instructorFilePath = Path('dataset/processed', 'instructors.csv')
 
 # read unitime dataset file
+# W (value.2) column stores department value
+# AB (offering) column stores course offerings
+# AG (scheduler) column stores department id for each course
+# AO (days) column stores the number of days the courses are held
+# AQ (length) column stores the number of hours of each course
 datasetUnitime = pd.read_excel(
-    datasetUnitimeFilePath, sheet_name='Sheet1', usecols="AO, AP, AQ")
-datasetUnitime.dropna(inplace=True)
+    datasetUnitimeFilePath, sheet_name='Sheet1', usecols="W, AB, AG, AO, AQ",   header=0, converters={"value.2": str})
+datasetUnitime.rename(columns = {'value.1':'value', "value.2": "value"},inplace =True) 
+
+# get unique list of unitime depts. Ideally, this list should be taken from AG column, but this column only has 1 department (3)
+unitimeDepts = list(set(datasetUnitime['value'].dropna()))
+
+# get unique list of unitime courses
+unitimeCourses = list(set(datasetUnitime['offering'].dropna()))
+
 
 # read instructor file
-instructorFile = pd.read_csv(instructorFilePath, usecols=[
-                             'instructors'], sep=";")
+instructorFile = pd.read_csv(instructorFilePath, usecols=['instructors'], sep=";")
 instructors = instructorFile["instructors"].to_list()
 
 # read illinois dataset file
 datasetIllinois = pd.read_csv(datasetIllinoisFilePath, usecols=[
-                              'Name', "CRN"], sep=",").drop_duplicates()
+                              'Name'], sep=",").drop_duplicates()
 coursesNames = datasetIllinois['Name'].to_list()
-coursesDepts = datasetIllinois['CRN'].to_list()
-
 
 # find how many times the courses are scheduled weekly
-# AO (days) column stores the number of days the courses are held
-# AQ (length) column stores the number of hours of each course
 def findWeeklyHours():
-    weeklySchedules = datasetUnitime['days']
-    durationInFiveMinutesUnit = datasetUnitime['length']
+    weeklySchedules = datasetUnitime['days'].dropna()
+    durationInFiveMinutesUnit = datasetUnitime['length'].dropna()
     weeklySchedulesCounts = list(
         map(lambda schedule: str(schedule).count("1"), weeklySchedules))
     durationInHours = list(
@@ -52,22 +59,14 @@ def findWeeklyHours():
 weeklyHours = findWeeklyHours()
 
 # get courses, removes &amp;, limit the length, remove duplicates 
-uniqueCoursesDepts = list()
-filteredCourses = list()
+uniqueCourses = list()
 for index, course in enumerate(coursesNames):
-    if course not in filteredCourses: 
-        filteredCourses.append(course)
-        uniqueCoursesDepts.append([course.replace("&amp;", "&"), coursesDepts[index]])
+    if course not in uniqueCourses: 
+        uniqueCourses.append(course)
 
-# separate the zipped courses & dept, get the first two digit of CRN as department names
-courses, depts=zip(*uniqueCoursesDepts[:len(weeklyHours)])
-courses, depts=(list(courses),list(depts))
-depts = list(map(lambda dept: str(dept)[:2], coursesDepts[:len(weeklyHours)]))
+uniqueCourses = list(map(lambda course:course.replace("&amp;", "&"), uniqueCourses))
 
-# generate a list of course instructor ids
-courseInstructors = list()
-for i in range(0, len(weeklyHours)):
-    courseInstructors.append(randrange(0, len(instructors), 1))
+courses = uniqueCourses[:len(unitimeCourses)]
 
 # Generate new csv file
 targetFilePath = Path("dataset/processed", "courses.csv")
@@ -78,8 +77,10 @@ writer = csv.writer(targetFile, dialect="excel", delimiter=";")
 writer.writerow(["courses", "weekly_hours", "instructors", "major"])
 i = 0
 while i != len(courses):
+    randomInstructor = randrange(0, len(instructors)-1, 1)
+    randomDept = randrange(0, len(unitimeDepts)-1, 1)
     writer.writerow([courses[i:i+1], weeklyHours[i:i+1],
-                    courseInstructors[i:i+1], depts[i:i+1]])
+                    randomInstructor, randomDept])
     i += 1
 targetFile.close()
 
