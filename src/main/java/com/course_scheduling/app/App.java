406e018f7b081883438e4f5c3f223d07752d2e64
Argi K <argikartika@gmail.com>
2022-01-06 21:54:25 +0100

    fix: fixed FileManager file, previously lost to refactoring

--- src/main/java/com/course_scheduling/app/App.java
+++ src/main/java/com/course_scheduling/app/App.java
@@ -1,14 +1,16 @@
+
 package com.course_scheduling.app;
 
 import com.course_scheduling.ga.Driver;
 
 /**
- * Main class Called in pom.xml
+ * Main class, called in pom.xml
  *
  */
 public class App {
 
     public static void main(String[] args) {
+        // run ga algorithm
         Driver driver = new Driver();
         driver.runAlgorithm();
     }
